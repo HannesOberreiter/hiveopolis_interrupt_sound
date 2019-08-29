69cc47c2ca65ad70ec148b39f1c96d3e2070840e
Hannes Oberreiter <office@btree.at>
2019-08-29 13:01:58 +0200

    push

---
+++ sinus.py
@@ -0,0 +1,85 @@
+### pyaudio:
+### http://people.csail.mit.edu/hubert/pyaudio/
+
+## ToneGenerator from:
+## https://markhedleyjones.com/projects/python-tone-generator
+
+import numpy
+import pyaudio
+import math
+
+class ToneGenerator(object):
+
+    def __init__(self, samplerate=44100, frames_per_buffer=4410):
+        self.p = pyaudio.PyAudio()
+        self.samplerate = samplerate
+        self.frames_per_buffer = frames_per_buffer
+        self.streamOpen = False
+
+    def sinewave(self):
+        if self.buffer_offset + self.frames_per_buffer - 1 > self.x_max:
+            # We don't need a full buffer or audio so pad the end with 0's
+            xs = numpy.arange(self.buffer_offset,
+                              self.x_max)
+            tmp = self.amplitude * numpy.sin(xs * self.omega)
+            out = numpy.append(tmp,
+                               numpy.zeros(self.frames_per_buffer - len(tmp)))
+        else:
+            xs = numpy.arange(self.buffer_offset,
+                              self.buffer_offset + self.frames_per_buffer)
+            out = self.amplitude * numpy.sin(xs * self.omega)
+        self.buffer_offset += self.frames_per_buffer
+        return out
+
+    def callback(self, in_data, frame_count, time_info, status):
+        if self.buffer_offset < self.x_max:
+            data = self.sinewave().astype(numpy.float32)
+            return (data.tostring(), pyaudio.paContinue)
+        else:
+            return (None, pyaudio.paComplete)
+
+    def is_playing(self):
+        if self.stream.is_active():
+            return True
+        else:
+            if self.streamOpen:
+                self.stream.stop_stream()
+                self.stream.close()
+                self.streamOpen = False
+            return False
+
+    def play(self, frequency, duration, amplitude):
+        self.omega = float(frequency) * (math.pi * 2) / self.samplerate
+        self.amplitude = amplitude
+        self.buffer_offset = 0
+        self.streamOpen = True
+        self.x_max = math.ceil(self.samplerate * duration) - 1
+        self.stream = self.p.open(format=pyaudio.paFloat32,
+                                  channels=1,
+                                  rate=self.samplerate,
+                                  output=True,
+                                  frames_per_buffer=self.frames_per_buffer,
+                                  stream_callback=self.callback)
+
+generator = ToneGenerator()
+
+# Frequency to start the sweep from
+frequency_start = 100
+# Frequency to end the sweep at
+frequency_end = 1000
+# Number of frequencies in the sweep
+num_frequencies = 18
+# Amplitude of the waveform
+amplitude = 0.50
+# Time (seconds) to play at each step
+step_duration = 5
+
+for frequency in numpy.logspace(math.log(frequency_start, 10),
+                                math.log(frequency_end, 10),
+                                num_frequencies):
+
+    print("Playing tone at {0:0.2f} Hz".format(frequency))
+    generator.play(frequency, step_duration, amplitude)
+    while generator.is_playing():
+        # record the sound?
+        pass
