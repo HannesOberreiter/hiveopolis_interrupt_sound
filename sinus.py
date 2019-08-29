25aec19905b7b0b6e87b5a60e7297cd6121f8998
Hannes Oberreiter <office@btree.at>
2019-08-29 14:44:14 +0200

    now with multiple runs

--- sinus.py
+++ sinus.py
@@ -1,15 +1,28 @@
-### pyaudio --- see link for installtion guide
-### http://people.csail.mit.edu/hubert/pyaudio/
-
-## ToneGenerator from:
-## https://markhedleyjones.com/projects/python-tone-generator
+#############################
+# Hiveopolis - frequency generator
+# for dance interrupt experiment
+#############################
 
 import numpy
+### pyaudio --- see link for installtion guide
+### http://people.csail.mit.edu/hubert/pyaudio/
 import pyaudio
 import math
 import time
 import random
+import os
 
+# define our clear function
+# https://www.geeksforgeeks.org/clear-screen-python/
+def clear():
+    # for windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = os.system('clear')
+
+# Tone Generator (https://markhedleyjones.com/projects/python-tone-generator)
 class ToneGenerator(object):
 
     def __init__(self, samplerate=44100, frames_per_buffer=4410):
@@ -63,28 +76,27 @@ class ToneGenerator(object):
                                   frames_per_buffer=self.frames_per_buffer,
                                   stream_callback=self.callback)
 
+# init our class object
 generator = ToneGenerator()
-
-# Frequency to start the sweep from
+# define start and endpoint of frequencies, with step in range
 frequency_start = 100
-# Frequency to end the sweep at
 frequency_end = 800
-# steps
 steps = 50
-# generate array
+# Time (seconds) to play at each step
+step_duration = 5
+# create list array of frequencies
 frequency_array = list(range(frequency_start, frequency_end, steps))
-# push to array special cases
+
+# add special cases to array
 frequency_array.extend([1000])
 frequency_array.extend([1500])
 frequency_array.extend([2000])
 frequency_array.extend([2500])
-
 # white noise
 frequency_array.extend([0])
 def white_noise():
     stream = pyaudio.PyAudio().open(format = pyaudio.paInt8,channels = 1,rate = 22050,output = True)
     for n in range(0,220000,1): stream.write(chr(int(random.random()*256)))
-
 # Kammerton A
 frequency_array.extend([440])
 # D5
@@ -92,28 +104,46 @@ frequency_array.extend([587.33])
 
 # Amplitude of the waveform
 amplitude = 0.50
-# Time (seconds) to play at each step
-step_duration = 5
 
-random.shuffle(frequency_array)
+# Number of runs
+runs = 4
 
-for x in range(0, len(frequency_array)):
-    print("Loop - " + str(x) + " / " +  str(len(frequency_array)))
-    if(frequency_array[x] == 0):
-        print("Playing white noise")
-        white_noise()
-    else:
-        if(frequency_array[x] == 440):
-            print("Playing concert pitch A")
-        elif(frequency_array[x] == 587.33):
-            print("Playing concert pitch A")
+# clear
+clear()
+
+for i in range(1, runs + 1):
+
+    # print starting sequence
+    print('Hiveopolis run: ' + str(i))
+    print('Starting in 5 seconds ...')
+    time.sleep(5)
+    clear()
+    # shuffle our frequencies in array
+    random.shuffle(frequency_array)
+
+    for x in range(0, len(frequency_array)):
+        # print current loop number
+        print("Loop - " + str(x+1) + " / " +  str(len(frequency_array)))
+        # special case white noise
+        if(frequency_array[x] == 0):
+            print("Playing white noise")
+            white_noise()
         else:
-            print("Playing tone at {0:0.2f} Hz".format(frequency_array[x]))
+            # custom prints for special cases
+            if(frequency_array[x] == 440):
+                print("Playing concert pitch A")
+            elif(frequency_array[x] == 587.33):
+                print("Playing concert pitch A")
+            else:
+                # standard print of current frequency
+                print("Playing tone at {0:0.2f} Hz".format(frequency_array[x]))
 
-        generator.play(frequency_array[x], step_duration, amplitude)
-        while generator.is_playing():
-            # record the sound?
-            pass
+            # use our generator class to generate the sound
+            generator.play(frequency_array[x], step_duration, amplitude)
+            while generator.is_playing():
+                # record the sound?
+                pass
 
-    print("Sleep 10 seconds")
-    time.sleep(10)
+        print("Sleep 10 seconds")
+        time.sleep(10)
+        clear()
