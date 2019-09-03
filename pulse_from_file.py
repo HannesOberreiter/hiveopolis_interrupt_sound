a215b3064208136ca82b27c44580a7ff28d8f202
Hannes Oberreiter <office@btree.at>
2019-09-03 17:00:22 +0200

    Updated pulse to use the 1 minute files

--- pulse_from_file.py
+++ pulse_from_file.py
@@ -11,9 +11,9 @@ import math
 import time
 import random
 import os
-import wave 
+import wave
 
-runs = 6
+runs = 4
 # define our clear function
 # https://www.geeksforgeeks.org/clear-screen-python/
 def clear():
@@ -27,17 +27,19 @@ def clear():
 # clear
 clear()
 
-#define stream chunk   
-chunk = 1024  
+#define stream chunk
+chunk = 1024
 
 
-file_array = ["16bit5000hz30sek1amp.wav","16bit5000hz30sek1amp_pulse09seksil01sec.wav", "16bit5000hz30sek00625amp_pulse09seksil01sec.wav", "16bit5000hz30sec00625amp.wav" ]
+#file_array = ["16bit5000hz30sek1amp.wav","16bit5000hz30sek1amp_pulse09seksil01sec.wav", "16bit5000hz30sek00625amp_pulse09seksil01sec.wav", "16bit5000hz30sec00625amp.wav" ]
+file_array = ["16bit500hz1min1amp.wav","16bit500hz1min1amp_pulse09seksil01sec.wav", "16bit5000hz1min1amp_pulse09seksil01sec.wav", "16bit5000hz1min1amp.wav" ]
+
 print('######  Hiveopolis ######')
 print('Starting in 4 minutes ...')
-time.sleep(240)
+#time.sleep(240)
 clear()
-#instantiate PyAudio  
-p = pyaudio.PyAudio() 
+#instantiate PyAudio
+p = pyaudio.PyAudio()
 
 for i in range(1, runs + 1):
 
@@ -48,43 +50,56 @@ for i in range(1, runs + 1):
     clear()
     # shuffle our frequencies in array
     random.shuffle(file_array)
-    
+    loop = 0
     for j in file_array:
         f = wave.open(r"sound_files/{}".format(j),"rb")
-        if j == "16bit5000hz30sek1amp.wav":
-            a = "(FRQ = 5000hz, AMP = 1)"
-        if j == "16bit5000hz30sek1amp_pulse09seksil01sec.wav":
-            a = "(FRQ = 500hz, AMP = 1, PULSE = 0.9:0.1sec)"
-        if j == "16bit5000hz30sec00625amp.wav":
-            a = "(FRQ = 5000hz, AMP = 0.0625)"
-        if j == "16bit5000hz30sek00625amp_pulse09seksil01sec.wav":
-            a = "(FRQ = 500hz, AMP = 0.0625, PULSE = 0.9:0.1sec)"
-        print("Playing {} for 30 seconds".format(a))
- 
-        #open stream  
-        stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
-                        channels = f.getnchannels(),  
-                        rate = f.getframerate(),  
-                        output = True)  
-        #read data  
-        data = f.readframes(chunk)  
 
-        #play stream  
-        while data:  
-            stream.write(data)  
-            data = f.readframes(chunk)  
+        # if j == "16bit5000hz30sek1amp.wav":
+        #     a = "(FRQ = 5,000hz, AMP = 1)"
+        # if j == "16bit5000hz30sek1amp_pulse09seksil01sec.wav":
+        #     a = "(FRQ = 5,000hz, AMP = 1, PULSE = 0.9:0.1sec)"
+        # if j == "16bit5000hz30sec00625amp.wav":
+        #     a = "(FRQ = 5,000hz, AMP = 0.0625)"
+        # if j == "16bit5000hz30sek00625amp_pulse09seksil01sec.wav":
+        #     a = "(FRQ = 5,000hz, AMP = 0.0625, PULSE = 0.9:0.1sec)"
 
-        #stop stream  
-        stream.stop_stream()  
-        stream.close()  
-        clear()
 
-        
+        if j == "16bit500hz1min1amp.wav":
+            a = "(FRQ = 500hz, AMP = 1, 60s)"
+        if j == "16bit500hz1min1amp_pulse09seksil01sec.wav":
+            a = "(FRQ = 500hz, AMP = 1, PULSE = 0.9:0.1sec, 60s)"
+        if j == "16bit5000hz1min1amp_pulse09seksil01sec.wav":
+            a = "(FRQ = 5,000hz, AMP = 1, PULSE = 0.9:0.1sec, 60s)"
+        if j == "16bit5000hz1min1amp.wav":
+            a = "(FRQ = 5,000hz, AMP = 1, 60s)"
+
+        loop = loop + 1
+        print("Run: " + str(i) + " - Loop: " + str(loop) + " / " +  str(len(file_array)))
+        print("Playing {}".format(a))
+
+        #open stream
+        stream = p.open(format = p.get_format_from_width(f.getsampwidth()),
+                        channels = f.getnchannels(),
+                        rate = f.getframerate(),
+                        output = True)
+        #read data
+        data = f.readframes(chunk)
+
+        #play stream
+        while data:
+            stream.write(data)
+            data = f.readframes(chunk)
+
+        #stop stream
+        stream.stop_stream()
+        stream.close()
+
+
         print("Vibration off: 30 seconds")
         time.sleep(30)
         clear()
 
-p.terminate() 
+p.terminate()
 print('######  Hiveopolis ######')
 print('######   Finished  ######')
 time.sleep(240)
